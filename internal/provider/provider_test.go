f903fd276e679f7a1699eb7ca7ae1797621c3d88
chrnorm <17420369+chrnorm@users.noreply.github.com>
2024-07-30 19:21:50 +0100

    fix lint

--- internal/provider/provider_test.go
+++ internal/provider/provider_test.go
@@ -1,8 +1,6 @@
 package provider
 
 import (
-	"testing"
-
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
 	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
 )
@@ -13,10 +11,4 @@ import (
 // reattach.
 var testAccProtoV6ProviderFactories = map[string]func() (tfprotov6.ProviderServer, error){
 	"cedar": providerserver.NewProtocol6WithError(New("test")()),
-}
-
-func testAccPreCheck(t *testing.T) {
-	// You can add code here to run prior to any test case execution, for example assertions
-	// about the appropriate environment variables being set are common to see in a pre-check
-	// function.
 }
