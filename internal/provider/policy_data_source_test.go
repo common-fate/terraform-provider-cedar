1709735d208a0e8c159ddfcbd8b732a11345c099
chrnorm <17420369+chrnorm@users.noreply.github.com>
2024-07-31 10:49:14 +0100

    Fix the annotation schema
    Also adds unit test coverage over the behaviour

--- internal/provider/policy_data_source_test.go
+++ internal/provider/policy_data_source_test.go
@@ -36,3 +36,38 @@ func TestPolicyDataSource_Simple(t *testing.T) {
 		},
 	})
 }
+
+func TestPolicyDataSource_Annotations(t *testing.T) {
+	resource.UnitTest(t, resource.TestCase{
+		TerraformVersionChecks: []tfversion.TerraformVersionCheck{
+			tfversion.SkipBelow(tfversion.Version1_5_0),
+		},
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: `
+				data "cedar_policyset" "test" {
+					policy {
+						annotation {
+							name = "advice"
+							value = "test"
+						}
+
+						effect = "permit"
+						any_principal = true
+						any_action = true
+						any_resource = true
+					}
+				}
+
+				output "test" {
+					value = data.cedar_policyset.test.text
+				}
+				`,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckOutput("test", "@advice(\"test\")\npermit (\n\tprincipal,\n\taction,\n\tresource\n);\n"),
+				),
+			},
+		},
+	})
+}
